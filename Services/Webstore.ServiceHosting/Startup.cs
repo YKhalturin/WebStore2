9abb605cc824b3671a5fec8a0befd76d46169e39
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2021-01-21 20:31:13 +0300

    Сonnect services to ServiceHosting

--- Services/Webstore.ServiceHosting/Startup.cs
+++ Services/Webstore.ServiceHosting/Startup.cs
@@ -10,6 +10,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using Webstore.Interfaces.Services;
+using Webstore.Services.Data;
+using Webstore.Services.Products.InCookies;
+using Webstore.Services.Products.InMemory;
+using Webstore.Services.Products.InSQL;
 
 namespace Webstore.ServiceHosting
 {
@@ -25,6 +34,37 @@ namespace Webstore.ServiceHosting
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<WebStoreDB>(
+                opt => opt.UseSqlServer(Configuration.GetConnectionString("Default")));
+            services.AddTransient<WebStoreDbInitializer>();
+
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<WebStoreDB>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(opt =>
+            {
+#if DEBUG
+                opt.Password.RequiredLength = 3;
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredUniqueChars = 3;
+#endif
+                opt.User.RequireUniqueEmail = false;
+                opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+                opt.Lockout.AllowedForNewUsers = true;
+                opt.Lockout.MaxFailedAccessAttempts = 10;
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+            });
+
+            services.AddScoped<IEmployeesData, InMemoryEmployeesData>();
+            services
+                .AddScoped<IProductData, SqlProductData>()
+                .AddScoped<ICartService, InCookiesCartService>()
+                .AddScoped<IOrderService, SqlOrderService>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
