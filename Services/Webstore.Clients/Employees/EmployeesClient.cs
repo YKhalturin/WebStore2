b53e4a204860233fc36346073c9fb0ee1a8d93f8
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2021-01-22 21:11:07 +0300

    Client for employees managing

---
+++ Services/Webstore.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Webstore.Clients.Base;
+using WebStore.Domain.Models;
+using Webstore.Interfaces.Services;
+
+namespace Webstore.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        private readonly ILogger<EmployeesClient> _Logger;
+
+        public EmployeesClient(IConfiguration Configuration, ILogger<EmployeesClient> Logger)
+            : base(Configuration, "api/employees") =>
+            _Logger = Logger;
+
+
+        public IEnumerable<Employee> Get() => Get<IEnumerable<Employee>>(Address);
+
+        public Employee Get(int id) => Get<Employee>($"{Address}/{id}");
+
+        public int Add(Employee employee) => Post(Address, employee).Content.ReadAsAsync<int>().Result;
+
+        public void Update(Employee employee)
+        {
+            _Logger.LogInformation("Редактирование сотрудника с id:{0}", employee.Id);
+            Put(Address, employee);
+        }
+
+        public bool Delete(int id) => Delete($"{Address}/{id}").IsSuccessStatusCode;
+    }
+}
