6c112c9db28959e22b261f4e0e912652dece3233
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2021-01-23 15:06:09 +0300

    WebApi controller template

---
+++ Services/Webstore.ServiceHosting/Controllers/ProductsApiController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using Webstore.Interfaces.Services;
+
+namespace Webstore.ServiceHosting.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsApiController : ControllerBase, IProductData
+    {
+        private readonly IProductData _ProductData;
+
+        public ProductsApiController(IProductData ProductData) => _ProductData = ProductData;
+
+        [HttpGet("sections")] // http://localhost:5000/api/products/sections
+        public IEnumerable<Section> GetSections() => _ProductData.GetSections();
+
+        [HttpGet("sections/{id}")] // http://localhost:5000/api/products/sections/5
+        public Section GetSectionById(int id) => _ProductData.GetSectionById(id);
+
+        [HttpGet("brands")] // http://localhost:5000/api/products/brands
+        public IEnumerable<Brand> GetBrands() => _ProductData.GetBrands();
+
+        [HttpGet("brands/{id}")] // http://localhost:5000/api/products/brands/5
+        public Brand GetBrandById(int id) => _ProductData.GetBrandById(id);
+
+        [HttpPost]
+        public IEnumerable<Product> GetProducts([FromBody] ProductFilter Filter) => _ProductData.GetProducts(Filter);
+
+        [HttpGet("{id}")] // http://localhost:5000/api/products/5
+        public Product GetProductById(int id) => _ProductData.GetProductById(id);
+
+    }
+}
