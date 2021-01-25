a4eaa75dfc050567d1fccfd1188a99c68ccf3235
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2021-01-25 12:09:13 +0300

    Creates interfaces for WebApi

---
+++ Services/Webstore.Interfaces/Services/Identity/IRolesClient.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Entities.Identity;
+
+namespace Webstore.Interfaces.Services.Identity
+{
+    public interface IRolesClient : IRoleStore<Role> { }
+}
