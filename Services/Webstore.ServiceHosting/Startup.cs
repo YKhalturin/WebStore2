31331fb32e8f7785f0e6df1680c05e3ff163822f
Yuriy Khalturin <yuriy.khalturin@arkadium.com>
2021-01-29 16:28:51 +0300

    Update launch settings

--- Services/Webstore.ServiceHosting/Startup.cs
+++ Services/Webstore.ServiceHosting/Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -58,19 +59,33 @@ namespace WebStore.ServiceHosting
                .AddScoped<IOrderService, SqlOrderService>();
 
             services.AddControllers();
+
+            const string webstore_api_xml = "WebStore.ServiceHosting.xml";
+            const string webstore_domain_xml = "WebStore.Domain.xml";
+            const string debug_path = "bin/Debug/net5.0";
+
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Webstore.ServiceHosting", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebStore.ServiceHosting", Version = "v1" });
+
+                c.IncludeXmlComments(webstore_api_xml);
+
+                if (File.Exists(webstore_domain_xml))
+                    c.IncludeXmlComments(webstore_domain_xml);
+                else if (File.Exists(Path.Combine(debug_path, webstore_domain_xml)))
+                    c.IncludeXmlComments(Path.Combine(debug_path, webstore_domain_xml));
             });
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, WebStoreDbInitializer db)
         {
+            db.Initialize();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Webstore.ServiceHosting v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebStore.ServiceHosting v1"));
             }
 
             app.UseRouting();
